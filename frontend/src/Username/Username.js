356ab794178af80ef51e13c268cc048a5ce4db2e
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-18 20:55:03 +0100

    Add change username form

--- frontend/src/Username/Username.js
+++ frontend/src/Username/Username.js
@@ -3,12 +3,36 @@ import React, { Component } from 'react';
 import './Username.css';
 
 class Username extends Component {
+  state = {
+    username: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ username: target.value });
+  };
+
+  handleSubmit = () => {};
+
   render() {
-    const { onLogout } = this.props;
+    const { auth, onLogout } = this.props;
+    const { username } = this.state;
+
     return (
       <div className="Username">
-        <p>My username is</p>
-        <button onClick={onLogout}>Logout</button>
+        <p>
+          My public address is<button onClick={onLogout}>Logout</button>
+        </p>
+
+        <p>My username is {auth || 'not set.'}</p>
+        <div>
+          <label htmlFor="username">Change username:</label>
+          <input
+            name="username"
+            onChange={this.handleChange}
+            value={username}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </div>
       </div>
     );
   }
