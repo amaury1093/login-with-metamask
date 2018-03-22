ebae1f558e08ef2fed041f5b9e116698e893e30c
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-22 16:18:01 +0100

    Change title

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MetaMask Login Demo</h1>
+          <h1 className="App-title">Welcome to Login with MetaMask Demo</h1>
         </header>
         <div className="App-intro">
           {auth ? (
