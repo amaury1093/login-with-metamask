533bac641b10e8e88e9a45212ff34b4756c86bce
Filip Š <filip.stamcar@hotmail.com>
2018-08-28 18:08:32 +0200

    Fix importing modules

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,6 +2,9 @@ import Sequelize from 'sequelize';
 
 import User from './models/user.model';
 
+const path = require('path');
+const os = require('os');
+
 const sequelize = new Sequelize(null, null, null, {
   dialect: 'sqlite',
   storage: path.join(os.tmpdir(), 'db.sqlite'),
