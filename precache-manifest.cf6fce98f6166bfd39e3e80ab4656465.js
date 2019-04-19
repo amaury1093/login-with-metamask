dd756045c74b3f7e15e791ae07151ffe067c87cf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 10:43:20 +0000

    Deploy amaurymartiny/login-with-metamask-demo to github.com/amaurymartiny/login-with-metamask-demo.git:gh-pages

---
+++ precache-manifest.cf6fce98f6166bfd39e3e80ab4656465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d89f75fb98f593e6d11f",
+    "url": "/static/js/main.76fa071d.chunk.js"
+  },
+  {
+    "revision": "a28779c4f9885a361373",
+    "url": "/static/js/2.9bc9ae28.chunk.js"
+  },
+  {
+    "revision": "d89f75fb98f593e6d11f",
+    "url": "/static/css/main.4792511f.chunk.css"
+  },
+  {
+    "revision": "0190d6ef8f28611e3298ffc97019e27f",
+    "url": "/index.html"
+  }
+];
