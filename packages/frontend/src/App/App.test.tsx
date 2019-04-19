71f7cdb2b24e571907feb91becad85999cf289f5
Amaury Martiny <amaury.martiny@protonmail.com>
2019-04-19 11:52:42 +0200

    test: Add test command for both packages

--- packages/frontend/src/App/App.test.tsx
+++ packages/frontend/src/App/App.test.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
