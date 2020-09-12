8bf9ce41adf443c128cbb2abf23a62c0e3f7dedf
anegg0 <gael.blanchemain@protonmail.com>
2020-09-12 13:36:18 -0700

    linting iteration #4

--- packages/frontend/src/Login/Login.tsx
+++ packages/frontend/src/Login/Login.tsx
@@ -5,6 +5,17 @@ import Web3 from 'web3';
 
 import { Auth } from '../types';
 
+declare global {
+  interface Web3 {
+    ethereum: unknown;
+  }
+}
+
+interface Window {
+  window: void;
+  alert: void;
+}
+
 interface Props {
   onLoggedIn: (auth: Auth) => void;
 }
@@ -22,7 +33,7 @@ export class Login extends React.Component<Props> {
   }: {
     publicAddress: string;
     signature: string;
-  }): Promise<any> =>
+  }): Promise<void> =>
     fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
       body: JSON.stringify({ publicAddress, signature }),
       headers: {
@@ -35,19 +46,21 @@ export class Login extends React.Component<Props> {
     const { onLoggedIn } = this.props;
 
     // Check if MetaMask is installed
-    if (!(window as any).ethereum) {
-      window.alert('Please install MetaMask first.');
-      return;
-    }
+    // if (!(window as any).ethereum) {
+    // if (!win.ethereum) {
+    //   window.alert('Please install MetaMask first.');
+    //   return;
+    // }
 
-    if (!web3) {
+    if (!window) {
       try {
         // Request account access if needed
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         await (window as any).ethereum.enable();
 
         // We don't know window.web3 version, so we use our own instance of Web3
         // with the injected provider given by MetaMask
-        web3 = new Web3((window as any).ethereum);
+        web3 = new Web3(window).ethereum;
       } catch (error) {
         window.alert('You need to allow MetaMask.');
         return;
@@ -76,6 +89,7 @@ export class Login extends React.Component<Props> {
       .then(this.handleSignMessage)
       // Send signature to backend on the /auth route
       .then(this.handleAuthenticate)
+      // ({ publicAddress, signature, }: { publicAddress: string; signature: string; }) => Promise<any>
       // Pass accessToken back to parent component (to save it in localStorage)
       .then(onLoggedIn)
       .catch((err) => {
@@ -91,20 +105,22 @@ export class Login extends React.Component<Props> {
     publicAddress: string;
     nonce: string;
   }): Promise<unknown> => {
-    try {
-      const signature = await web3!.eth.personal.sign(
-        `I am signing my one-time nonce: ${nonce}`,
-        publicAddress,
-        '' // MetaMask will ignore the password argument here
-      );
+    if (web3) {
+      try {
+        const signature = await web3.eth.personal.sign(
+          `I am signing my one-time nonce: ${nonce}`,
+          publicAddress,
+          '' // MetaMask will ignore the password argument here
+        );
 
-      return { publicAddress, signature };
-    } catch (err) {
-      throw new Error('You need to sign the message to be able to log in.');
+        return { publicAddress, signature };
+      } catch (err) {
+        throw new Error('You need to sign the message to be able to log in.');
+      }
     }
   };
 
-  handleSignup = (publicAddress: string): Promise<any> => {
+  handleSignup = (publicAddress: string): Promise<void> => {
     return fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
       body: JSON.stringify({ publicAddress }),
       headers: {
@@ -114,7 +130,7 @@ export class Login extends React.Component<Props> {
     }).then((response) => response.json());
   };
 
-  render() {
+  render(): JSX.Element {
     const { loading } = this.state;
     return (
       <div>
