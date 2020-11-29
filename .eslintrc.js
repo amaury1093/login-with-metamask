378a04f97b5e78095faddc5a722c6abc5a55f249
Amaury Martiny <amaury.martiny@protonmail.com>
2020-11-29 02:07:08 +0100

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
 		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
 		'@typescript-eslint/no-unsafe-return': 'off',
+		'@typescript-eslint/no-floating-promises': 'off',
 	},
 };
