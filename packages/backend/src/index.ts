1631e2e0027e3994f3809e823df77e88a4f7765e
Amaury <1293565+amaurym@users.noreply.github.com>
2023-10-04 17:26:10 +0200

    chore: Small fixes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 // Mount REST on /api
 app.use('/api', services);
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 app.listen(port, () =>
 	console.log(`Express app listening on localhost:${port}`)
